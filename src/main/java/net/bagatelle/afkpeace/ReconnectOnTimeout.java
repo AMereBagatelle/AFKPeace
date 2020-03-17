6d5db5b060899a41e65e0250ea6f3d3f26a79018
AMereBagatelle <brennenputh@gmail.com>
2020-03-17 16:59:08 -0400

    Creating isActive in ReconnectOnTimeout and getters and setters

--- src/main/java/net/bagatelle/afkpeace/ReconnectOnTimeout.java
+++ src/main/java/net/bagatelle/afkpeace/ReconnectOnTimeout.java
@@ -1,1 +1,17 @@
 package net.bagatelle.afkpeace;
+
+public class ReconnectOnTimeout {
+    private boolean isActive;
+
+    public ReconnectOnTimeout() {
+        isActive = false;
+    }
+
+    public void setActive(boolean setTo) {
+        isActive = setTo;
+    }
+
+    public boolean getActive() {
+        return isActive;
+    }
+}
