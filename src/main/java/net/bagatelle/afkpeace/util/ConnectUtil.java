5252f19fbc118e7085701465048b2934b9517bdf
AMereBagatelle <brennenputh@gmail.com>
2020-03-18 07:04:23 -0400

    Now gets data from the server correctly

---
+++ src/main/java/net/bagatelle/afkpeace/util/ConnectUtil.java
@@ -0,0 +1,11 @@
+package net.bagatelle.afkpeace.util;
+
+public class ConnectUtil {
+    
+    public void connectToServer(String serverName) {
+        if(serverName != null) {
+            
+        }
+    }
+
+}
