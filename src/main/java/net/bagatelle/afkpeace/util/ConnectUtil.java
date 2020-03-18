8f855049a380cb9457dee592d1db331374e4689c
AMereBagatelle <brennenputh@gmail.com>
2020-03-18 18:03:29 -0400

    Final commit for today... I think I am going crazy

--- src/main/java/net/bagatelle/afkpeace/util/ConnectUtil.java
+++ src/main/java/net/bagatelle/afkpeace/util/ConnectUtil.java
@@ -1,5 +1,6 @@
 package net.bagatelle.afkpeace.util;
 
+import net.bagatelle.afkpeace.constants.ReconnectionConstants;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.network.ServerInfo;
