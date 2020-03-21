746d1e15b4368d0666c227ecb2bca5263901ce84
AMereBagatelle <brennenputh@gmail.com>
2020-03-21 18:15:13 -0400

    Making time between reconnection attempts somewhat configurable

--- src/main/java/net/bagatelle/afkpeace/constants/ReconnectionConstants.java
+++ src/main/java/net/bagatelle/afkpeace/constants/ReconnectionConstants.java
@@ -1,5 +1,6 @@
 package net.bagatelle.afkpeace.constants;
 
 public class ReconnectionConstants {
-    public static final int maxReconnectTries = 1000;
+    public static final int maxReconnectTries = 3;
+    public static final int secondsBetweenReconnectAttempts = 5;
 }
