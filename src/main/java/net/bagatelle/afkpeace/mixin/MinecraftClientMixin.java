0aca1908ffe468317f3290f4e2ac0ecca84a271a
AMereBagatelle <brennenputh@gmail.com>
2020-03-21 21:46:41 -0400

    Adding MinecraftClientMixin to start with the player shadow button addition

---
+++ src/main/java/net/bagatelle/afkpeace/mixin/MinecraftClientMixin.java
@@ -0,0 +1,30 @@
+package net.bagatelle.afkpeace.mixin;
+
+import java.util.Arrays;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.bagatelle.afkpeace.miscellaneous.PlayerShadowPauseMenuScreen;
+import net.minecraft.client.MinecraftClient;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(method="openPauseMenu", at=@At("HEAD"), cancellable=true)
+    public void addPlayerShadowButton(boolean bl, CallbackInfo cbi) {
+        System.out.println("Mixin worked");
+        MinecraftClient mc = MinecraftClient.getInstance();
+        if (mc.currentScreen == null && mc.player.networkHandler.getCommandDispatcher().findNode(Arrays.asList("player")) != null) {
+            boolean bl2 = mc.isIntegratedServerRunning() && !mc.getServer().isRemote();
+            if (bl2) {
+                mc.openScreen(new PlayerShadowPauseMenuScreen(!bl));
+                mc.getSoundManager().pauseAll();
+            } else {
+                mc.openScreen(new PlayerShadowPauseMenuScreen(true));
+            }
+            cbi.cancel();
+        }
+    }
+}
