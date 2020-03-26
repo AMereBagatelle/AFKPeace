c3eb8ca488c44b3c3840150c7e327d278dc6fcff
AMereBagatelle <brennenputh@gmail.com>
2020-03-26 07:33:14 -0400

    Setting the maxReconnectTries with a command now works

--- src/main/java/net/bagatelle/afkpeace/settings/SettingsManager.java
+++ src/main/java/net/bagatelle/afkpeace/settings/SettingsManager.java
@@ -1,12 +1,13 @@
 package net.bagatelle.afkpeace.settings;
 
 import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class SettingsManager {
     // * Settings File. DO NOT CHANGE UNLESS YOU KNOW WHAT YOU ARE DOING
@@ -26,9 +27,13 @@ public class SettingsManager {
         try {
             Properties prop = new Properties();
 
-            inputStream = new FileInputStream(settingsFilePath);
+            inputStream = SettingsManager.class.getClassLoader().getResourceAsStream(settingsFilePath);
 
-            prop.load(inputStream);
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new IOException("Could not find properties file");
+            }
 
             inputStream.close();
 
@@ -40,10 +45,27 @@ public class SettingsManager {
     }
 
     public static void writeSetting(String setting, String setpoint) throws IOException {
+        InputStream inputStream;
         OutputStream outputStream;
         Properties prop = new Properties();
 
-        outputStream = new FileOutputStream(settingsFilePath);
+        inputStream = SettingsManager.class.getClassLoader().getResourceAsStream(settingsFilePath);
+
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new IOException("Could not get properties file");
+        }
+
+        inputStream.close();
+
+        URL settingsFileURL = SettingsManager.class.getClassLoader().getResource(settingsFilePath);
+        try {
+            File settingsFile = new File(settingsFileURL.toURI());
+            outputStream = new FileOutputStream(settingsFile);
+        } catch (URISyntaxException e) {
+            throw new IOException("URIException");
+        }
 
         prop.setProperty(setting, setpoint);
 
