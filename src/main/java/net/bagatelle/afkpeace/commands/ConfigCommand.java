12472ba59319644bc506e5b4cc3259103121272a
AMereBagatelle <brennenputh@gmail.com>
2020-03-26 07:33:14 -0400

    Setting the maxReconnectTries with a command now works

--- src/main/java/net/bagatelle/afkpeace/commands/ConfigCommand.java
+++ src/main/java/net/bagatelle/afkpeace/commands/ConfigCommand.java
@@ -1,5 +1,50 @@
 package net.bagatelle.afkpeace.commands;
 
-public class ConfigCommand {
-    
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+import io.github.cottonmc.clientcommands.ArgumentBuilders;
+import io.github.cottonmc.clientcommands.ClientCommandPlugin;
+import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import net.bagatelle.afkpeace.settings.SettingsManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
+
+import static io.github.cottonmc.clientcommands.ArgumentBuilders.*;
+
+import java.io.IOException;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+
+public class ConfigCommand implements ClientCommandPlugin {
+
+    @Override
+    public void registerCommands(CommandDispatcher<CottonClientCommandSource> dispatcher) {
+        LiteralArgumentBuilder<CottonClientCommandSource> afkpeace = literal("afkpeace")
+            .then(literal("maxReconnectTries")
+                .then(argument("tries", integer())
+                    .executes(ctx -> {
+                        MinecraftClient mc = MinecraftClient.getInstance();
+                        try {
+                            String tries = Integer.toString(IntegerArgumentType.getInteger(ctx, "tries"));
+                            SettingsManager.writeSetting("maxReconnectTries", tries);
+                            mc.inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("MaxReconnectTries set to: " + tries));
+                            return 1;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            mc.inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("Was not able to set setting."));
+                            return -1;
+                        }
+                    })
+                ))
+            .executes(ctx -> {
+                return 1;
+            });
+
+        dispatcher.register(afkpeace);
+    }
+
 }
