24bea7224d3a051b9520020666ee2a0b8d015138
AMereBagatelle <brennenputh@gmail.com>
2020-03-28 20:41:20 -0400

    Resolving unused imports

--- src/main/java/net/bagatelle/afkpeace/settings/SettingsManager.java
+++ src/main/java/net/bagatelle/afkpeace/settings/SettingsManager.java
@@ -4,16 +4,9 @@ import java.util.Properties;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public class SettingsManager {
     // * Settings File. DO NOT CHANGE UNLESS YOU KNOW WHAT YOU ARE DOING
