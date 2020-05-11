306e9d107c6b2fcb3bd82a7f92acaa75a900b2fc
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-10 20:37:18 -0400

    Finishing wiping and also rewriting things

--- src/main/java/amerebagatelle/github/io/afkpeace/miscellaneous/ReconnectTestThread.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/miscellaneous/ReconnectTestThread.java
@@ -6,9 +6,12 @@ import java.net.UnknownHostException;
 
 import amerebagatelle.github.io.afkpeace.AFKPeace;
 import amerebagatelle.github.io.afkpeace.settings.SettingsManager;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.network.ServerAddress;
 
+@Environment(EnvType.CLIENT)
 public class ReconnectTestThread extends Thread {
 
     private int canReconnect;
@@ -23,22 +26,7 @@ public class ReconnectTestThread extends Thread {
 
     // Tries to connect to the server using a socket as many times as is set, and returns if it could connect
     public void run() {
-        for (int i = 0; i <= SettingsManager.maxReconnectTries; i++) {
-            try {
-                Thread.sleep(SettingsManager.secondsBetweenReconnectionAttempts * 1000);
-                Socket connectionAttempt = new Socket(serverAddress.getAddress(), serverAddress.getPort());
-                connectionAttempt.close();
-                canReconnect = 1;
-                break;
-            } catch (UnknownHostException e) {
-            } catch (IOException e) {
-            } catch (InterruptedException e) {
-            }
-        }
-        if(canReconnect != 1) {
-            canReconnect = 2;
-        }
-        synchronized(this) {AFKPeace.stateVariables.canReconnect = canReconnect;}
+
     }
 
 }
