e0a70451d6fc0ef9c018eb4af4e651fa7c30e5cf
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-10 20:42:20 -0400

    Started ConnectionManager

--- src/main/java/amerebagatelle/github/io/afkpeace/ConnectionManager.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/ConnectionManager.java
@@ -1,8 +1,15 @@
 package amerebagatelle.github.io.afkpeace;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.network.ServerInfo;
 import net.minecraft.network.ServerAddress;
 
+@Environment(EnvType.CLIENT)
 public class ConnectionManager {
     private final MinecraftClient minecraft;
 
@@ -10,6 +17,11 @@ public class ConnectionManager {
         this.minecraft = minecraft;
     }
 
-    public void connectToServer(ServerAddress target) {
+    public void connectToServer(ServerInfo target) {
+        this.minecraft.openScreen(new ConnectScreen(new MultiplayerScreen(new TitleScreen()), this.minecraft, target));
+    }
+
+    public void disconnectFromServer() {
+
     }
 }
