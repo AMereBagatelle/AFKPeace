083d7ec114a9093cf7a2dd0bffa8536f7f4b10c3
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-14 08:40:06 -0400

    Finishing ReconnectThread

--- src/main/java/amerebagatelle/github/io/afkpeace/miscellaneous/ReconnectThread.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/miscellaneous/ReconnectThread.java
@@ -1,10 +1,14 @@
 package amerebagatelle.github.io.afkpeace.miscellaneous;
 
+import amerebagatelle.github.io.afkpeace.AFKPeace;
 import amerebagatelle.github.io.afkpeace.settings.SettingsManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.network.ServerAddress;
+
+import java.io.IOException;
+import java.net.Socket;
 
 @Environment(EnvType.CLIENT)
 public class ReconnectThread extends Thread {
@@ -25,7 +29,20 @@ public class ReconnectThread extends Thread {
     // Tries to connect to the server using a socket as many times as is set, and returns if it could connect
     public void run() {
         for (int i = 0; i < timesToAttempt; i++) {
-
+            Socket connectionAttempt = null;
+            try {
+                connectionAttempt = new Socket(serverAddress.getAddress(), serverAddress.getPort());
+                connectionAttempt.close();
+                synchronized (this) {
+                    AFKPeace.isReconnecting = true;
+                }
+                break;
+            } catch (IOException e) {
+                try {
+                    Thread.sleep(secondsBetweenAttempts);
+                } catch (InterruptedException ignored) {
+                }
+            }
 
         }
     }
