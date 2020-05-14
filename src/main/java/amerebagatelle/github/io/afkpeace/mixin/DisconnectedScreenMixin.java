d73bd19405f2a3e466d1107db8a0ffbfaf231300
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-14 16:40:45 -0400

    Starting on screen

--- src/main/java/amerebagatelle/github/io/afkpeace/mixin/DisconnectedScreenMixin.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/mixin/DisconnectedScreenMixin.java
@@ -1,15 +1,22 @@
 package amerebagatelle.github.io.afkpeace.mixin;
 
 import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.NarratorManager;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DisconnectedScreen.class)
-public class DisconnectedScreenMixin {
+public abstract class DisconnectedScreenMixin extends Screen {
+    protected DisconnectedScreenMixin(Text title) {
+        super(NarratorManager.EMPTY);
+    }
+
     @Inject(method = "init", at = @At("HEAD"))
     public void onInit(CallbackInfo ci) {
-
+        this.addButton();
     }
 }
