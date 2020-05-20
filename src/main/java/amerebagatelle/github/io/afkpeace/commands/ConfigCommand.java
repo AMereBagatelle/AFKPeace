6b34ccac2e7014b2258b6ec78dab53add2ecf2dd
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-20 17:10:51 -0400

    Updating to 20w21a

--- src/main/java/amerebagatelle/github/io/afkpeace/commands/ConfigCommand.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/commands/ConfigCommand.java
@@ -26,46 +26,46 @@ public class ConfigCommand implements ClientCommandPlugin {
         LiteralArgumentBuilder<CottonClientCommandSource> afkpeace = literal("afkpeace")
                 .then(literal("reconnectEnabled")
                         .executes(ctx -> {
-                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectEnabled is set to" + SettingsManager.loadSetting("reconnectEnabled")));
+                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectEnabled is set to" + SettingsManager.loadSetting("reconnectEnabled")), MinecraftClient.getInstance().player.getUuid());
                             return 1;
                         })
                         .then(argument("setpoint", bool())
                                 .executes(ctx -> {
                                     SettingsManager.writeSetting("reconnectEnabled", Boolean.toString(BoolArgumentType.getBool(ctx, "setpoint")));
-                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectEnabled set to" + SettingsManager.loadSetting("reconnectEnabled")));
+                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectEnabled set to" + SettingsManager.loadSetting("reconnectEnabled")), MinecraftClient.getInstance().player.getUuid());
                                     return 1;
                                 })))
                 .then(literal("damageLogoutEnabled")
                         .executes(ctx -> {
-                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("damageLogoutEnabled is set to " + SettingsManager.loadSetting("damageLogoutEnabled")));
+                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("damageLogoutEnabled is set to " + SettingsManager.loadSetting("damageLogoutEnabled")), MinecraftClient.getInstance().player.getUuid());
                             return 1;
                         })
                         .then(argument("setpoint", bool())
                                 .executes(ctx -> {
                                     SettingsManager.writeSetting("damageLogoutEnabled", Boolean.toString(BoolArgumentType.getBool(ctx, "setpoint")));
-                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("damageLogoutEnabled set to " + SettingsManager.loadSetting("damageLogoutEnabled")));
+                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("damageLogoutEnabled set to " + SettingsManager.loadSetting("damageLogoutEnabled")), MinecraftClient.getInstance().player.getUuid());
                                     return 1;
                                 })))
                 .then(literal("secondsBetweenReconnectionAttempts")
                         .executes(ctx -> {
-                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("secondsBetweenReconnectionAttempts is set to " + SettingsManager.loadSetting("secondsBetweenReconnectAttempts")));
+                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("secondsBetweenReconnectionAttempts is set to " + SettingsManager.loadSetting("secondsBetweenReconnectAttempts")), MinecraftClient.getInstance().player.getUuid());
                             return 1;
                         })
                         .then(argument("setpoint", integer())
                                 .executes(ctx -> {
                                     SettingsManager.writeSetting("secondsBetweenReconnectionAttempts", Integer.toString(IntegerArgumentType.getInteger(ctx, "setpoint")));
-                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("secondsBetweenReconnectionAttempts set to " + SettingsManager.loadSetting("secondsBetweenReconnectAttempts")));
+                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("secondsBetweenReconnectionAttempts set to " + SettingsManager.loadSetting("secondsBetweenReconnectAttempts")), MinecraftClient.getInstance().player.getUuid());
                                     return 1;
                                 })))
                 .then(literal("reconnectAttemptNumber")
                         .executes(ctx -> {
-                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectAttemptNumber is set to " + SettingsManager.loadSetting("reconnectAttemptNumber")));
+                            MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectAttemptNumber is set to " + SettingsManager.loadSetting("reconnectAttemptNumber")), MinecraftClient.getInstance().player.getUuid());
                             return 1;
                         })
                         .then(argument("setpoint", integer())
                                 .executes(ctx -> {
                                     SettingsManager.writeSetting("reconnectAttemptNumber", Integer.toString(IntegerArgumentType.getInteger(ctx, "setpoint")));
-                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectAttemptNumber set to " + SettingsManager.loadSetting("reconnectAttemptNumber")));
+                                    MinecraftClient.getInstance().inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText("reconnectAttemptNumber set to " + SettingsManager.loadSetting("reconnectAttemptNumber")), MinecraftClient.getInstance().player.getUuid());
                                     return 1;
                                 })))
                 .then(literal("afkmode")
