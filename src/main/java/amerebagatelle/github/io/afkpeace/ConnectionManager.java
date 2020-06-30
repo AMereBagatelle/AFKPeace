a12144c9f035a4fa7a2cd1e2fc945567062b40ef
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-30 06:55:00 -0400

    Fixing reconnectOnDamageLogout

--- src/main/java/amerebagatelle/github/io/afkpeace/ConnectionManager.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/ConnectionManager.java
@@ -10,6 +10,7 @@ import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
@@ -28,6 +29,8 @@ public class ConnectionManager {
 
     // Handling the reconnect feature
     public void startReconnect(ServerInfo target) {
+        this.minecraft.getNetworkHandler().getConnection().disconnect(new LiteralText("Reconnecting"));
+        this.minecraft.disconnect();
         reconnectThread = new ReconnectThread(target);
         reconnectThread.start();
         this.minecraft.openScreen(new DisconnectedScreen(new MultiplayerScreen(new TitleScreen()), "AFKPeaceReconnection", new TranslatableText("reconnect.reason")));
@@ -37,14 +40,27 @@ public class ConnectionManager {
         connectToServer(AFKPeace.currentServerEntry);
     }
 
+    public void cancelReconnect() {
+        try {
+            reconnectThread.join();
+        } catch (InterruptedException ignored) {
+        }
+        this.minecraft.openScreen(new DisconnectedScreen(new MultiplayerScreen(new TitleScreen()), "AFKPeaceDisconnect", new LiteralText("Couldn't reconnect.")));
+    }
+
     // Regular connecting utilities
     public void connectToServer(ServerInfo target) {
         this.minecraft.openScreen(new ConnectScreen(new MultiplayerScreen(new TitleScreen()), this.minecraft, target));
     }
 
     public void disconnectFromServer(Text reason) {
-        isDisconnecting = !Boolean.parseBoolean(SettingsManager.loadSetting("reconnectOnDamageLogout"));
-        this.minecraft.getNetworkHandler().getConnection().disconnect(reason);
-        this.minecraft.openScreen(new DisconnectedScreen(new MultiplayerScreen(new TitleScreen()), "AFKPeaceDisconnect", reason));
+        if (!Boolean.parseBoolean(SettingsManager.loadSetting("reconnectOnDamageLogout"))) {
+            isDisconnecting = true;
+            this.minecraft.getNetworkHandler().getConnection().disconnect(reason);
+            this.minecraft.disconnect();
+            this.minecraft.openScreen(new DisconnectedScreen(new MultiplayerScreen(new TitleScreen()), "AFKPeaceDisconnect", reason));
+        } else {
+            startReconnect(AFKPeace.currentServerEntry);
+        }
     }
 }
