9c48b375e6d089511bf5a01ae93cf8ab06e1a2c8
AMereBagatelle <themerebagatelle@gmail.com>
2020-07-02 13:28:08 -0400

    Added version log message

--- src/main/java/amerebagatelle/github/io/afkpeace/AFKPeace.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/AFKPeace.java
@@ -4,17 +4,22 @@ import amerebagatelle.github.io.afkpeace.settings.SettingsManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Environment(EnvType.CLIENT)
 public class AFKPeace implements ClientModInitializer {
 	private static final MinecraftClient mc = MinecraftClient.getInstance();
+	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static ServerInfo currentServerEntry;
 
 	@Override
 	public void onInitializeClient() {
+		LOGGER.info("AFKPeace " + FabricLoader.getInstance().getModContainer("afkpeace").get().getMetadata().getVersion() + " Initialized");
 		SettingsManager.initSettings();
 	}
 }
