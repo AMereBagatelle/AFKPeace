1103aea5710c97e0e17c1e85c4a4bb2168403ea9
AMereBagatelle <themerebagatelle@gmail.com>
2020-07-31 09:23:46 -0400

    Adding time of disconnect feature.  Fixes #10

--- src/main/java/amerebagatelle/github/io/afkpeace/mixin/DisconnectedScreenMixin.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/mixin/DisconnectedScreenMixin.java
@@ -6,7 +6,9 @@ import amerebagatelle.github.io.afkpeace.settings.SettingsManager;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.NarratorManager;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,8 +16,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Mixin(DisconnectedScreen.class)
 public abstract class DisconnectedScreenMixin extends Screen {
+    private String timeOfDisconnect;
+
     protected DisconnectedScreenMixin(Text title) {
         super(NarratorManager.EMPTY);
     }
@@ -25,8 +32,14 @@ public abstract class DisconnectedScreenMixin extends Screen {
      */
     @Inject(method = "init", at = @At("TAIL"))
     public void onInit(CallbackInfo ci) {
+        timeOfDisconnect = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         if (!Boolean.parseBoolean(SettingsManager.loadSetting("reconnectEnabled")) && AFKPeace.currentServerEntry != null) {
             this.addButton(new ButtonWidget(width / 2 - 100, this.height - 30, 200, 20, new TranslatableText("reconnect"), (buttonWidget) -> ConnectionManager.INSTANCE.connectToServer(AFKPeace.currentServerEntry)));
         }
+    }
+
+    @Inject(method = "render", at = @At("TAIL"))
+    public void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        this.drawCenteredString(matrices, textRenderer, I18n.translate("afkpeace.disconnectscreen.time", timeOfDisconnect), width / 2, height - 50, 16777215);
     }
 }
