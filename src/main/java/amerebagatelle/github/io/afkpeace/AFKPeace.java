eb4c25d19cc5c78893370fce1502118e03c73946
AMereBagatelle <themerebagatelle@gmail.com>
2020-09-03 11:41:08 -0400

    Remove unneeded variable

--- src/main/java/amerebagatelle/github/io/afkpeace/AFKPeace.java
+++ src/main/java/amerebagatelle/github/io/afkpeace/AFKPeace.java
@@ -5,14 +5,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Environment(EnvType.CLIENT)
 public class AFKPeace implements ClientModInitializer {
-	private static final MinecraftClient mc = MinecraftClient.getInstance();
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static ServerInfo currentServerEntry;
