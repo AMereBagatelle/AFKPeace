1f41121ef713bc50dcced48f6ff91c7d5b96061d
AMereBagatelle <themerebagatelle@gmail.com>
2020-09-22 17:33:29 -0400

    Refactor GUI to be more clean

---
+++ src/main/java/amerebagatelle/github/io/afkpeace/gui/ConfigTextWidget.java
@@ -0,0 +1,35 @@
+package amerebagatelle.github.io.afkpeace.gui;
+
+import amerebagatelle.github.io.afkpeace.settings.SettingsManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+
+public class ConfigTextWidget extends TextFieldWidget {
+    private final String option;
+
+    public ConfigTextWidget(TextRenderer textRenderer, int x, int y, int width, int height, String option) {
+        super(textRenderer, x, y, width, height, new LiteralText(""));
+        this.option = option;
+        setText(SettingsManager.loadSetting(option));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+        drawStringWithShadow(matrices, textRenderer, option, x, y - 10, 16777215);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    public void saveValue() {
+        SettingsManager.writeSetting(option, getText());
+    }
+
+    @Override
+    public boolean charTyped(char chr, int keyCode) {
+        SettingsManager.writeSetting(option, getText());
+        return super.charTyped(chr, keyCode);
+    }
+}
